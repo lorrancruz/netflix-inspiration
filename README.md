e0aa4633c407da2177c54708ab5565aa43a23ae5
Lorran Cruz <112294310+lorrancruz@users.noreply.github.com>
2022-09-12 08:22:42 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,4 @@
+# AluraFlix v1- ID3
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/lorrancruz/pen/zYjraBB](https://codepen.io/lorrancruz/pen/zYjraBB).
+
---
+++ dist/script.js
@@ -0,0 +1,9 @@
+var listafilmes = [
+  "https://img.elo7.com.br/product/original/2679A20/big-poster-filme-matrix-lo03-tamanho-90x60-cm-geek.jpg",
+  "https://img.elo7.com.br/product/original/25FA5F0/big-poster-filme-alita-anjo-de-combate-lo01-tamanho-90x60-cm-poster.jpg",
+  "https://flxt.tmsimg.com/assets/p17387519_p_v13_ae.jpg"
+];
+
+for (var i = 0; i < listafilmes.length; i++) {
+  document.write("<img src=" + listafilmes[i] + ">");
+}
